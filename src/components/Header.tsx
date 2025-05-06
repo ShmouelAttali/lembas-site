d939da628e22d7b601384f74652ff98bd1972ba0
Ester Attali <ester.attali@gmail.com>
2025-05-06 18:31:19 +0300

    stupid fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React from 'react';
-import Image from 'next/image';
 import Link from 'next/link';
 import MainNav from './MainNav';
 import CartMinimized from './CartMinimized';
@@ -25,16 +24,12 @@ const Header: FC<HeaderProps> = ({suffix}) => {
                         srcSet="/lembasLogo.svg 358w, /lembasLogo-vertical.svg 595w"
                         sizes="(max-width: 600px) 595px, 358px"
                         alt="למבס logo"
-                        // width={}
-                        // height={65}
                     />
                     <h1 className="suffix">{suffix}</h1>
                 </Link>
 
-                {/*<div className="header-button-items">*/}
                     <MainNav/>
                     {loading && <div className="small-spinner" aria-label="Loading"></div>}
-                {/*</div>*/}
             </div>
         </header>
     );
