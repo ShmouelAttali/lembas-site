f1502e6d70ec2552740d471c3bb4cde22447555f
Shmouel Attali <shmouel.attali@tigoenergy.com>
2025-10-23 14:54:49 +0300

    Initialize `notes` field in the checkout default state.
    Signed-off-by: Shmouel Attali <shmouel.attali@tigoenergy.com>

--- src/app/checkout/CheckoutPageClient.tsx
+++ src/app/checkout/CheckoutPageClient.tsx
@@ -40,6 +40,7 @@ export default function CheckoutPageClient({dates}: { dates: Date[] }) {
 
             return {
                 ...base,
+                notes: '',
                 orderDate: null
             };
         }
